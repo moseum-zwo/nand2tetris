dd21b7edb880290a47c4884a94f549fddc568d34
Moritz Linder <m.linder@posteo.de>
2022-04-22 19:22:35 +0200

    Improve Tokenizer to work with Regex

--- projects/JackCompiler/src/main/java/JackTokenizer.java
+++ projects/JackCompiler/src/main/java/JackTokenizer.java
@@ -2,131 +2,115 @@ import token.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JackTokenizer {
 
-    private static String CARRIER = "";
+    public enum KEYWORD {CLASS,
+        METHOD,FUNCTION,CONSTRUCTOR,
+        INT,BOOLEAN,CHAR,VOID,
+        VAR,STATIC,FIELD,
+        LET,DO,IF,ELSE,WHILE,
+        RETURN,
+        TRUE,FALSE,NULL,
+        THIS}
 
     private static boolean blockComment = false;
 
+    private static Pattern tokenPatterns;
+    private static String keyWordReg;
+    private static String symbolReg;
+    private static String intReg;
+    private static String strReg;
+    private static String idReg;
+
+    private static final HashMap<String,KEYWORD> keyWordMap = new HashMap<>();
+
+    static {
+
+        keyWordMap.put("class",KEYWORD.CLASS);keyWordMap.put("constructor",KEYWORD.CONSTRUCTOR);keyWordMap.put("function",KEYWORD.FUNCTION);
+        keyWordMap.put("method",KEYWORD.METHOD);keyWordMap.put("field",KEYWORD.FIELD);keyWordMap.put("static",KEYWORD.STATIC);
+        keyWordMap.put("var",KEYWORD.VAR);keyWordMap.put("int",KEYWORD.INT);keyWordMap.put("char",KEYWORD.CHAR);
+        keyWordMap.put("boolean",KEYWORD.BOOLEAN);keyWordMap.put("void",KEYWORD.VOID);keyWordMap.put("true",KEYWORD.TRUE);
+        keyWordMap.put("false",KEYWORD.FALSE);keyWordMap.put("null",KEYWORD.NULL);keyWordMap.put("this",KEYWORD.THIS);
+        keyWordMap.put("let",KEYWORD.LET);keyWordMap.put("do",KEYWORD.DO);keyWordMap.put("if",KEYWORD.IF);
+        keyWordMap.put("else",KEYWORD.ELSE);keyWordMap.put("while",KEYWORD.WHILE);keyWordMap.put("return",KEYWORD.RETURN);
+    }
+
     public List<Token> tokenizeFile(File file) throws FileNotFoundException {
         Scanner scanner = new Scanner(file);
+        StringBuilder preprocessed = new StringBuilder();
         List<Token> tokens = new ArrayList<>();
         while (scanner.hasNextLine()) {
-            String currentLine = scanner.nextLine();
-            if (currentLine.contains("//")) {
-                currentLine = currentLine.split("//")[0];
-            }
-            String currentLineHelper = "";
-            if (currentLine.contains("/*")) {
-                currentLineHelper = currentLine.split("/\\*")[0];
-                blockComment = true;
-            }
-            if (currentLine.contains("*/")) {
-                currentLineHelper = currentLineHelper + " " + currentLine.substring(currentLine.indexOf("*/") + 2);
-                blockComment = false;
-            }
-            if (!currentLineHelper.equals("")) {
-                currentLine = currentLineHelper;
-            }
+            String currentLine = noComment(scanner.nextLine());
 
             if (!currentLine.isBlank() && !blockComment) {
-                String[] words = currentLine.split(" ");
-                for (String currentWord : words) {
-                    if (currentWord.contains("\t")) {
-                        currentWord = currentWord.replace("\t", "");
-                    }
-                    analyzeCurrentWord(tokens, currentWord);
-                }
+                preprocessed.append(currentLine).append("\n");
             }
         }
+
+        initRegs();
+
+        Matcher matcher = tokenPatterns.matcher(preprocessed);
+
+        while (matcher.find()){
+            Token token;
+            String tokenValue = matcher.group();
+            if (tokenValue.matches(keyWordReg)) {
+                token = new Keyword(tokenValue);
+            } else if (tokenValue.matches(symbolReg)) {
+                token = new Symbol(tokenValue);
+            } else if (tokenValue.matches(strReg)) {
+                token = new StringConstant(tokenValue);
+            } else if (tokenValue.matches(intReg)) {
+                token = new IntegerConstant(tokenValue);
+            } else if (tokenValue.matches(idReg)) {
+                token = new Identifier(tokenValue);
+            } else {
+                throw new RuntimeException("No matching pattern");
+            }
+            tokens.add(token);
+        }
+
         return tokens;
     }
 
-    private void analyzeCurrentWord(List<Token> tokens, String currentWord) {
-        if (isKeyword(currentWord)) {
-            tokens.add(new Keyword(currentWord));
-        } else if (isIntegerConstant(currentWord)) {
-            tokens.add(new IntegerConstant(currentWord));
-        } else if (isStringConstant(currentWord)) {
-            tokens.add(new StringConstant(currentWord));
-        } else if (isFirstWordOfStringConstant(currentWord)) {
-            CARRIER = currentWord;
-        } else if (isLastWordOfStringConstant(currentWord)) {
-            String word = CARRIER + " " + currentWord;
-            tokens.add(new StringConstant(word.substring(1, word.length() - 1)));
-            CARRIER = "";
-        } else if (isSymbol(currentWord)) {
-            tokens.add(new Symbol(currentWord));
-        } else if (lastCharIsSemicolon(currentWord)) {
-            analyzeCurrentWord(tokens, currentWord.substring(0, currentWord.length() - 1));
-            tokens.add(new Symbol(";"));
-        } else if (containsSymbol(currentWord)) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < currentWord.length(); i++) {
-                String currentLetter = currentWord.substring(i, i + 1);
-                if (!isSymbol(currentLetter)) {
-                    sb.append(currentLetter);
-                } else {
-                    //end identifier
-                    analyzeCurrentWord(tokens, sb.toString());  //wrong
-                    tokens.add(new Symbol(currentLetter));
-                    sb = new StringBuilder();
-                }
-            }
-            if (!sb.toString().isBlank()) {
-                analyzeCurrentWord(tokens, sb.toString());
-            }
-        } else if (isPartOfStringConstant()) {
-            CARRIER = CARRIER + " " + currentWord;
-        } else {
-            if (!currentWord.isEmpty()) {
-                tokens.add(new Identifier(currentWord));
-            }
+    private void initRegs() {
+        keyWordReg = "";
+
+        for (String seg: keyWordMap.keySet()){
+            keyWordReg += seg + "|";
         }
+
+        symbolReg = "[&*+()./,\\-\\];~}|{>=\\[<]";
+        intReg = "[0-9]+";
+        strReg = "\"[^\"\n]*\"";
+        idReg = "[a-zA-Z_]\\w*";
+
+        tokenPatterns = Pattern.compile(idReg + "|" + keyWordReg + symbolReg + "|" + intReg + "|" + strReg);
     }
 
-    private boolean isLastWordOfStringConstant(String currentWord) {
-        return currentWord.endsWith("\"") && !currentWord.substring(0, currentWord.length() - 1).contains("\"");
-    }
-
-    private boolean isFirstWordOfStringConstant(String currentWord) {
-        return currentWord.startsWith("\"") && !currentWord.substring(1).contains("\"");
-    }
-
-    private boolean isPartOfStringConstant() {
-        return !CARRIER.equals("");
-    }
-
-    private boolean lastCharIsSemicolon(String word) {
-        return word.endsWith(";");
-    }
-
-    private boolean isStringConstant(String word) {
-        return word.startsWith("\"") && word.endsWith("\"");
-    }
-
-    private boolean containsSymbol(String currentWord) {
-        return Symbol.allSymbols().stream().anyMatch(currentWord::contains);
-    }
-
-    private boolean isSymbol(String word) {
-        return Symbol.allSymbols().contains(word);
-    }
-
-    private boolean isIntegerConstant(String word) {
-        try {
-            Integer.parseInt(word);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
+    private String noComment(String currentLine) {
+        if (currentLine.contains("//")) {
+            currentLine = currentLine.split("//")[0];
         }
-    }
-
-    private boolean isKeyword(String word) {
-        return Keyword.allKeywords().contains(word);
+        if (currentLine.contains("\t")) {
+            currentLine = currentLine.replace("\t", "");
+        }
+        String currentLineHelper = "";
+        if (currentLine.contains("/*")) {
+            currentLineHelper = currentLine.split("/\\*")[0];
+            blockComment = true;
+        }
+        if (currentLine.contains("*/")) {
+            currentLineHelper = currentLineHelper + " " + currentLine.substring(currentLine.indexOf("*/") + 2);
+            blockComment = false;
+        }
+        if (!currentLineHelper.equals("")) {
+            currentLine = currentLineHelper;
+        }
+        return currentLine;
     }
 }
