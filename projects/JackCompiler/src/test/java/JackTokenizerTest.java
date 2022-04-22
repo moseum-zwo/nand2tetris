4843205f7fce4bc3bbba5a6639a6742ca930c008
Moritz Linder <m.linder@posteo.de>
2022-04-22 14:54:14 +0200

    Add test for potential improvement of Tokenizer

--- projects/JackCompiler/src/test/java/JackTokenizerTest.java
+++ projects/JackCompiler/src/test/java/JackTokenizerTest.java
@@ -85,4 +85,53 @@ class JackTokenizerTest {
                 new Symbol(";")
                 );
     }
+
+    @Test
+    void testTokenizing3() throws IOException {
+        File tempFile = new File("/home/moritz/Documents/nand2tetris/projects/JackCompiler/src/test/resources/TempFile.txt");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+        String line1 = "do Output.printString(\"Test 1: expected result: 5; actual result:_\");";
+        String line2 = "do Output.printInt(b[2]);";
+        String line3 = "do Output.println();";
+        writer.write(line1);
+        writer.newLine();
+        writer.write(line2);
+        writer.newLine();
+        writer.write(line3);
+        writer.newLine();
+
+        writer.close();
+
+        List<Token> tokens = jackTokenizer.tokenizeFile(tempFile);
+
+        System.out.println(tokens);
+
+//        tempFile.delete();
+//
+//        assertThat(tokens).containsExactly(new Identifier("myVar"),
+//                new Symbol("="),
+//                new Identifier("arr"),
+//                new Symbol("["),
+//                new IntegerConstant("777"),
+//                new Symbol("]"),
+//                new Symbol(";"),
+//                new Keyword("var"),
+//                new Identifier("String"),
+//                new Identifier("string"),
+//                new Symbol("="),
+//                new Identifier("func"),
+//                new Symbol("("),
+//                new IntegerConstant("123"),
+//                new Symbol(","),
+//                new Identifier("a_2"),
+//                new Symbol(")"),
+//                new Symbol(";"),
+//                new Keyword("let"),
+//                new Identifier("string"),
+//                new Symbol("="),
+//                new StringConstant(line1),
+//                new Symbol(";")
+//                );
+    }
 }
